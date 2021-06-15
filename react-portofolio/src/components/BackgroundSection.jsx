b277e69661e86761e8a7b476481b0114366643a1
Amir Najafi <dyloge@gmail.com>
2021-06-15 18:51:24 +0430

    4. Background section

---
+++ react-portofolio/src/components/BackgroundSection.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export default function BackgroundSection() {
+    return (
+        <div className="background">
+            <h2>My <span>Background</span></h2>
+            <div className="field">
+                <h4>Theatre</h4>
+                <div className="explanation">
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, aperiam!</p>
+                </div>
+            </div>
+            <div className="field">
+                <h4>Condensed matter physics</h4>
+                <div className="explanation">
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, aperiam!</p>
+                </div>
+            </div>
+            <div className="field">
+                <h4>Siesmology</h4>
+                <div className="explanation">
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, aperiam!</p>
+                </div>
+            </div>
+            <div className="field">
+                <h4>Philology</h4>
+                <div className="explanation">
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, aperiam!</p>
+                </div>
+            </div>
+            <div className="field">
+                <h4>Digital humanities</h4>
+                <div className="explanation">
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, aperiam!</p>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
--- react-portofolio/src/pages/AboutUs.jsx
+++ react-portofolio/src/pages/AboutUs.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
-import ServicesSection from '../components/ServicesSection'
+import ServicesSection from '../components/ServicesSection';
+import BackgroundSection from '../components/BackgroundSection';
 
 export default function AboutUs() {
     return (
         <div>
             <AboutSection/>
             <ServicesSection/>
+            <BackgroundSection/>
             
             
         </div>
