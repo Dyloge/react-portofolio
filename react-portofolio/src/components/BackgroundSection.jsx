a314d8a21a2e602c1e56e83ebb3df8c88a200141
Amir Najafi <dyloge@gmail.com>
2021-06-18 20:24:13 +0430

    9. Styling background section (2)

--- react-portofolio/src/components/BackgroundSection.jsx
+++ react-portofolio/src/components/BackgroundSection.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import { StyledAbout, StyledDescription } from '../styles';
-import ScriptTag from 'react-script-tag';
 
 
-const Demo = props => (
-    <ScriptTag type="text/javascript" src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"/>
-    )
+
+
 
 export default function BackgroundSection() {
     return (
@@ -15,18 +13,39 @@ export default function BackgroundSection() {
             <StyledAccordionContainer>
                 <StyledAccordionItem>
                     <StyledInput  type="checkbox" id="chck1"></StyledInput>
-                    <StyledQuestion className='tab-lable' for="chck1">Item 1</StyledQuestion>
+                    <StyledQuestion className='tab-label' for="chck1">Theatre</StyledQuestion>
                     <StyledAnswer className="tab-content">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum, reiciendis!
+                        Lorem<span className='colored-text'>dolor sit amet</span> ipsum dolor sit amet consectetur, adipisicing elit. Ipsum, reiciendis!
                     </StyledAnswer>
                 </StyledAccordionItem>
+
                 <StyledAccordionItem>
                     <StyledInput type="checkbox" id="chck2"></StyledInput>
-                    <StyledQuestion className='tab-lable' for="chck2">Item 2</StyledQuestion>
+                    <StyledQuestion className='tab-label' for="chck2">Condensed matter physics</StyledQuestion>
                     <StyledAnswer className="tab-content">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. A, in!
+                        Lorem ipsum dolor sit amet consectetur adipisicing <span className='colored-text'>dolor sit amet</span>elit. A, in!
                     </StyledAnswer>
                 </StyledAccordionItem>
+
+                <StyledAccordionItem>
+                    <StyledInput  type="checkbox" id="chck3"></StyledInput>
+                    <StyledQuestion className='tab-label' for="chck3">Seismology</StyledQuestion>
+
+                    <StyledAnswer className="tab-content">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum,<span className='colored-text'>dolor sit amet</span> reiciendis!
+                    </StyledAnswer>
+                </StyledAccordionItem>
+
+                <StyledAccordionItem>
+                    <StyledInput  type="checkbox" id="chck4"></StyledInput>
+                    <StyledQuestion className='tab-label' for="chck4">Digital humanities</StyledQuestion>
+                    
+                    <StyledAnswer className="tab-content">
+                        Lorem ipsum<span className='colored-text'>dolor sit amet</span> dolor sit amet consectetur, adipisicing elit. Ipsum, reiciendis!
+                        
+                    </StyledAnswer>
+                </StyledAccordionItem>
+
             </StyledAccordionContainer>
         </StyledFaq>
     )
@@ -46,17 +65,26 @@ const StyledInput=styled.input `
 const StyledAccordionContainer=styled.div `
   border-radius: 8px;
   overflow: hidden;
-  box-shadow: 0 4px 4px -2px rgba(0,0,0,0.5);
+
 `
 const StyledAccordionItem=styled.div `
     width: 100%;
+    margin: 10px;
     color: white;
     overflow: hidden;
+    .tab-label::after {
+        font-family: "Font Awesome 5 Free";
+	content: '\f0dd';
+	font-weight: bold;
+    color: #b43030;
+    }
     input:checked {
   + .tab-label {
-    background: darken($midnight, 10%);
+
     &::after {
       transform: rotate(90deg);
+      content: '\f0de';
+      color: #b43030;
     }
   }
   ~ .tab-content {
@@ -66,28 +94,21 @@ const StyledAccordionItem=styled.div `
 }
 `
 const StyledQuestion=styled.label `
-    display: flex;
-    justify-content: space-between;
-    padding: 1em;
-    font-weight: bold;
-    cursor: pointer;
-    &:hover{
-        background: #b43030;
-        color: white;
-    }
-    &::after {
-      content: "\276F";
-      width: 1em;
-      height: 1em;
-      text-align: center;
-      transition: all .35s;
-    }
+text-transform: uppercase;
+display: block;
+cursor: pointer;
+
+
 
 
 `
 const StyledAnswer=styled.div`
     max-height: 0;
-    padding: 0 1em;
-    transition: all .35s;
+    padding: 0 10em;
+    transition: all .5s ease;
+
+`
+const StyledLine=styled.hr `
+border: 1px solid #797979;
 
 `
